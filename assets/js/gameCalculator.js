47f4d6197fac6f0e06535c088c43af62fb4a655f
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-09-24 09:21:13 +0200

    Updating the gameCalculator, fixing a typo in the gameUi.js, cleaning up index.html. Adding explaining comments both in GameUi.js and gameCalculator.js. Adding a paper image in index.html.

--- assets/js/gameCalculator.js
+++ assets/js/gameCalculator.js
@@ -1,57 +1,44 @@
 
 //Here I define the selection of game choices
-const selection_Of_Game_Choices = ["Rock", "Paper", "Scissor"]; 
+const selection_Of_Game_Choices = ["Rock", "Paper", "Scissors"]; 
 
+//Here I create the computer choice random generator
 let computerChoice = () => {
-	let random_Number_Generator = [Math.floor(Math.random() * selection_Of_Game_Choices.length)];
-	return selection_Of_Game_Choices[random_Number_Generator]}
-
-console.log(computerChoice());
-
-
-
-
-
-
-
-	//start the game
-
-const calculate_Game_Outcome = (playerChoice, computerChoice) => {
-	//Update Text
-	const winner = document.querySelector(".winner");
-	//Checking for a tie
-	if (playerChoice === computerChoice) {
-		winner.textContent = "It is a tie";
-		return;
-	}
-	//Check for Rock
-	if (playerChoice === "rock") {
-		if (computerChoice === "scissors") {
-			winner.textContent = "Player Wins";
-			pScore++;
-			updateScore();
-			return;
-		} else {
-			winner.textContent = "Computer Wins";
-			cScore++;
-			updateScore();
-			return;
-		}
-	}
-				
-				
-				
-				
-				
-				
-				
-
-
-				//Here I create the computer choice
-				
-
-			};
-
+let random_Number_Generator = [Math.floor(Math.random() *  selection_Of_Game_Choices.length)];
+return selection_Of_Game_Choices[random_Number_Generator]
+}
 			
 
-			
+const playGame = (humanPlayerChoice) => {
+//console.log(`Jag spelar mot datorn med ${humanPlayerChoice}`);
+const computer = computerChoice();
+//console.log(`Datorn spelar med ${computer}`);
+        
+	if (humanPlayerChoice === computer){
+			console.log(`Oavgjort`);
+			console.log(`Människan spelar med ${humanPlayerChoice}`)
+			console.log(`Datorn spelar med ${computer}`)        
+			winner.textContent = "It is a draw. We are keeping the Aliens at bay for now..."
+	} else if (
+		(humanPlayerChoice === 'Paper' && computer === 'Rock') ||
+		(humanPlayerChoice === 'Scissors' && computer === 'Paper') ||
+		(humanPlayerChoice === 'Rock' && computer === 'Scissors' )){
+		console.log(`Människan spelar med ${humanPlayerChoice}`)
+		console.log(`Datorn spelar med ${computer}`)
+		console.log(`människa vinner`);
+		winner.textContent = "Congratulations Human, You Have Saved Mankind!"
+	} else {
+		
+		console.log(`Människan spelar med ${humanPlayerChoice}`)
+		console.log(`Datorn spelar med ${computer}`)
+		console.log(`Datorn vinner`)
+		winner.textContent = "The Aliens win, Mankind has fallen!!"
+	};
+
+
+				
+				
+				
+				
+
+	}
--- assets/js/gameUi.js
+++ assets/js/gameUi.js
@@ -1,14 +1,25 @@
+// Here I am creating the basic functionality to click on the images.  The element ids are fetched from div elements in index.html. 
+//After defining the const that gets the corresponding element id when clicking on respective image, I create the function that when clicking on an image,
+//the game is played with the argument of available game chocies. To avoid eventual confusion, the id is spelled in small letters and the playGame argument in capital
+
+//This is where the connection between the image in the url and the game function is first initiated. I am Creating a variable that fetches
+//the element Id of the div element that holds the image.
+
 const rock = document.getElementById("rock");
 
-rock.addEventListener("click", () => {console.log("you clicked on the rock")
-});
+rock.addEventListener("click", () => {
+  playGame('Rock')})
+
+
 
 const paper = document.getElementById("paper");
 
-paper.addEventListener("click", () => {console.log("you clicked on the paper")
-});
+paper.addEventListener("click", () => {
+  playGame('Paper')})
+
+
 
 const scissors = document.getElementById("scissors");
 
-scissors.addEventListener("click", () => {console.log("you clicked on the scissors")
-});
+scissors.addEventListener("click", () => {
+playGame('Scissors')})
