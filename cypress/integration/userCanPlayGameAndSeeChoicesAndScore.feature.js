44b258f3782a2b24af5bf66c56bbf7a443f240a3
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-09-25 13:23:36 +0200

    Adding a test for score board and updating readme with future updates.

--- cypress/integration/userCanPlayGameAndSeeChoicesAndScore.feature.js
+++ cypress/integration/userCanPlayGameAndSeeChoicesAndScore.feature.js
@@ -1,25 +1,36 @@
-describe('Visitor can visit page and see content', () => {
-  it('user can visit page ', () => {
-    cy.visit('/')
+describe("Visitor can visit page and see content", () => {
+  it("user can visit page ", () => {
+    cy.visit("/");
   });
 
-  it('visitor can see content on page ', () => {
-    cy.visit('/')
-    cy.get('h1').should('contain', 'RPS - A GAME THAT WILL DECIDE THE FATE OF MANKIND');
-    cy.get('h1').should('contain', 'WELCOME HUMAN. YOUR CHOICE WILL DETERMINE THE FATE OF PLANET EARTH. WHAT CHOOSE YOU?');
-  });
-});
-
-describe('User can play the game', () => {
-it('chose their throw', () => {
-cy.visit('/')
-cy.get('div#rock').click();
-cy.get('h2#playerchoice').should('contain','You chose Rock')
-cy.get('div#paper').click();
-cy.get('h2#playerchoice').should('contain','You chose Paper')
-cy.get('div#scissors').click();
-cy.get('h2#playerchoice').should('contain','You chose Scissors')
+  it("visitor can see content on page ", () => {
+    cy.visit("/");
+    cy.get("h1").should(
+      "contain",
+      "RPS - A GAME THAT WILL DECIDE THE FATE OF MANKIND"
+    );
+    cy.get("h1").should(
+      "contain",
+      "WELCOME HUMAN. YOUR CHOICE WILL DETERMINE THE FATE OF PLANET EARTH. WHAT CHOOSE YOU?"
+    );
   });
 });
 
-  
+describe("User can play the game", () => {
+  it("chose their throw", () => {
+    cy.visit("/");
+    cy.get("div#rock").click();
+    cy.get("h2#playerchoice").should("contain", "You chose Rock");
+    cy.get("div#paper").click();
+    cy.get("h2#playerchoice").should("contain", "You chose Paper");
+    cy.get("div#scissors").click();
+    cy.get("h2#playerchoice").should("contain", "You chose Scissors");
+  });
+});
+
+describe("User can see the game score", () => {
+  it("displays a score", () => {
+    cy.get("h4#playerscore");
+    cy.get("h4#computerscore");
+  });
+});
