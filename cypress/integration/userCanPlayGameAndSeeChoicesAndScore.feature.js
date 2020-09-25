d71fb07e1c685cf089abb97b950b60b2241be519
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-09-25 13:09:49 +0200

    Adding feature specs and, deleting example.json and updating README.MD

--- cypress/integration/userCanPlayGameAndSeeChoicesAndScore.feature.js
+++ cypress/integration/userCanPlayGameAndSeeChoicesAndScore.feature.js
@@ -1,23 +1,25 @@
 describe('Visitor can visit page and see content', () => {
   it('user can visit page ', () => {
     cy.visit('/')
-  })
+  });
 
-    it('visitor can see content on page ', () => {
-      cy.visit('/')
-      cy.get('h1').should('contain', 'RPS - A GAME THAT WILL DECIDE THE FATE OF MANKIND');
-      cy.get('h1').should('contain', 'WELCOME HUMAN. YOUR CHOICE WILL DETERMINE THE FATE OF PLANET EARTH. WHAT CHOOSE YOU?');
-    })
+  it('visitor can see content on page ', () => {
+    cy.visit('/')
+    cy.get('h1').should('contain', 'RPS - A GAME THAT WILL DECIDE THE FATE OF MANKIND');
+    cy.get('h1').should('contain', 'WELCOME HUMAN. YOUR CHOICE WILL DETERMINE THE FATE OF PLANET EARTH. WHAT CHOOSE YOU?');
+  });
+});
 
 describe('User can play the game', () => {
-it('user can visit page ', () => {
-  cy.visit('/')
-})
-
-
-
-  it('User can play the game', () => {
-    cy.get().click();
+it('chose their throw', () => {
+cy.visit('/')
+cy.get('div#rock').click();
+cy.get('h2#playerchoice').should('contain','You chose Rock')
+cy.get('div#paper').click();
+cy.get('h2#playerchoice').should('contain','You chose Paper')
+cy.get('div#scissors').click();
+cy.get('h2#playerchoice').should('contain','You chose Scissors')
   });
-})
+});
 
+  
